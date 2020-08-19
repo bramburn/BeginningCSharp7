261ff96df83b5d1ab635a7bf0fe7561fd9dd53f0
Bhavesh Ramburn <nitrogen@gmail.com>
2020-08-19 19:26:35 +0100

    ss

--- Chapter02/ConsoleApplication1/ConsoleApplication1/Program.cs
+++ Chapter02/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,18 +1,25 @@
-﻿using System;
+﻿#region Using directives
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+
 using System.Threading.Tasks;
+using static System.Console;
+#endregion
 
 namespace ConsoleApplication1
 {
     class Program
     {
+       
+
         static void Main(string[] args)
         {
-            // Output text to the screen.
-            Console.WriteLine("The first app in Beginning Visual C# 2017!");
-            Console.ReadKey();
+            // tuple mess about
+
+            ReadKey();
         }
     }
 }
